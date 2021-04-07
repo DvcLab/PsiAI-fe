fb16ca28da3d519de06b6ec8a51f2ec8cdc3954c
GoodNight <goodnightlilanhui@gmail.com>
2021-04-07 22:19:04 +0800

    perf: websocket

--- src/components/DvcAI/container-item.vue
+++ src/components/DvcAI/container-item.vue
@@ -1,6 +1,4 @@
 <script>
-import SockJS from  'sockjs-client';
-import Stomp from 'stompjs';
 import Swal from "sweetalert2";
 import LoaderContainer from "@/components/DvcAI/loader-container";
 
@@ -119,21 +117,11 @@ export default {
             return false;
         }
     },
-    created() {
-        // this.initWebSocket();
-    },
     mounted() {
-        this.initWebSocket();
-        // this.initWebSocket2();
-    },
-    beforeDestroy() {
-        // 页面离开时断开连接,清除定时器
-        // this.disconnect();
-        // clearInterval(this.timer);
+        if(this.status.text !== 'Deleted') this.initWebSocket();
     },
     destroyed() {
-        this.websock.close();
-        // this.websocketclose();
+        if(this.websock) this.websock.close();
     },
     methods: {
         // 打开JupyterLab页面
@@ -162,9 +150,7 @@ export default {
         // 初始化websocket
         initWebSocket() {
 
-            // const wsuri = "ws://j.dvclab.com:50000/_containers?id=" + this.container.id;
             const wsuri = process.env.VUE_APP_WS_URL + "_containers?id=" + this.container.id;
-            // const wsuri = "ws://j.dvclab.com:50000/_containers?id=5ba337c60902ae7ae71443a7bf956c62";
             this.websock = new WebSocket(wsuri, this.$keycloak.token);
             this.websock.onmessage = this.websocketonmessage;
             this.websock.onopen = this.websocketonopen;
@@ -198,57 +184,8 @@ export default {
         // 关闭
         websocketclose(e) {
             console.log('websocket error', e)
-            // this.websock.close();
         },
-        // ws初始化
-        initWebSocket2() {
-            this.connection();
-            let that= this;
-            // 断开重连机制,尝试发送消息,捕获异常发生时重连
-            this.timer = setInterval(() => {
-                try {
-                    that.stompClient.send("test");
-                } catch (err) {
-                    console.log("断线了: " + err);
-                    that.connection();
-                }
-            }, 5000);
-        },
-        //连接ws后台 
-        connection() {
-            const token = this.$keycloak.token;
-            const wsurl = process.env.VUE_APP_WS_API;
-            // 建立连接对象
-            let socket = new SockJS(wsurl);
-            // let socket = new SockJS('ws://j.dvclab.com:50000/', null, {transports: ['xhr-streaming'], headers: {'Authorization': 'Bearer ' + token}});
-            // 获取STOMP子协议的客户端对象
-            this.stompClient = Stomp.over(socket);
-            // 定义客户端的认证信息,按需求配置
-            let headers = {
-                Authorization: 'Bearer ' + token 
-            }
-            // 向服务器发起websocket连接
-            this.stompClient.connect(headers,() => {
-                this.stompClient.subscribe('_containers?host_id=' + this.container.id, (msg) => { // 订阅服务端提供的某个topic
-                    console.log('广播成功')
-                    console.log(msg);  // msg.body存放的是服务端发送给我们的信息
-                }, headers);
-                // this.stompClient.send("/app/chat.addUser",
-                //     headers,
-                //     JSON.stringify({sender: '',chatType: 'JOIN'}),
-                // )   //用户加入接口
-            }, (err) => {
-                // 连接发生错误时的处理函数
-                console.log('失败')
-                console.log(err);
-            });
-        },
-        // 断开ws连接
-        disconnect() {
-            if (this.stompClient) {
-                this.stompClient.disconnect();
-            }
-        },
+
         // 容器创建成功提醒
         successMsg() {
             Swal.fire(
@@ -262,6 +199,7 @@ export default {
                 }
             })
         },
+        
         // 容器创建失败提醒
         errorMsg() {
             Swal.fire(
@@ -366,9 +304,3 @@ export default {
     </div>
 </LoaderContainer>
 </template>
-
-<style scoped>
-.container-btn {
-    font-size: 1.2rem;
-}
-</style>
