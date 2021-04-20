cb883ecbc2f86bf45b48f2fc3d9b3929472edde7
GoodNight <goodnightlilanhui@gmail.com>
2021-04-20 22:24:07 +0800

    feat: add prismjs

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  plugins: [
+    [
+      "prismjs",
+      {
+        languages: ["javascript", "css", "markup"],
+        plugins: ["line-numbers"],
+        theme: "okaidia",
+        css: true
+      }
+    ]
   ]
 }
