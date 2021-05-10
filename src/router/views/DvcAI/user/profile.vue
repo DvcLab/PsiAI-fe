dcfff205e1eb6eed19c6eafc003a1b7218cb660f
刘芊怡 <liuqianyi16@163.com>
2021-05-11 02:21:16 +0800

    feat
    更新了个人信息管理页，将包含个人信息的那一列中的两个框合并成了一个框

--- src/router/views/DvcAI/user/profile.vue
+++ src/router/views/DvcAI/user/profile.vue
@@ -3,6 +3,7 @@ import Layout from "../../../layouts/main";
 import PageHeader from "@/components/page-header";
 import appConfig from "@/app.config";
 import { mapState } from 'vuex';
+
 /**
  * 容器列表
  */
@@ -31,17 +32,19 @@ export default {
           return state.auth.currentUser
         } else {
           return {
-            attributes: {
-              headimgurl: [],
-            },
-            username: ''
+            avatar_url:"",
+            name: ''
           }
         }
       } 
     }),
     avatarUrl() {
-      return this.userInfo.attributes.headimgurl ? this.userInfo.attributes.headimgurl[0] : require('@/assets/images/users/avatar-1.jpg')
-    }
+      return this.userInfo.avatar_url ? this.userInfo.avatar_url : require('@/assets/images/users/avatar-1.jpg')
+    },
+    // 是否是管理员
+    isAdmin() {
+      return this.$keycloak.realmAccess.roles.includes("DOCKHUB_ADMIN");
+    },
    },
   mounted() {
 
@@ -59,146 +62,7 @@ export default {
       <PageHeader :title="title" :items="items" />
       <div class="row">
         
-        <!--左边这一列-->
-        <div class="col-xl-4">
-          <!--左上角带头像的框-->
-          <div class="card text-center">
-            <div class="card-body">
-              <div v-if="!avatarUrl" class="avatar-sm mx-auto mb-4">
-                <span
-                  class="avatar-title rounded-circle bg-soft bg-primary text-primary font-size-16"
-                  >{{ userInfo.username.charAt(0) }}</span
-                >
-              </div>
-              <div v-if="avatarUrl" class="mb-4">
-                <img
-                  class="rounded-circle avatar-sm"
-                  :src="`${avatarUrl}`"
-                  alt
-                />
-              </div>
-              <h5 class="font-size-15 mb-1">
-                <a href="javascript: void(0);" class="text-dark">{{
-                  userInfo.username
-                }}</a>
-              </h5>
-              <p class="text-muted">这个人很懒，什么也没有留下~</p>
-              <!--
-              <div>
-                <div class="mt-4">
-                  <a href class="btn btn-primary btn-sm">
-                    Edit Profile
-                  </a>
-                </div>
-              </div>
-              -->
-              <div class="text-muted">
-                <ul class="list-inline mb-0">
-                    <li class="list-inline-item me-3">
-                      <h5
-                        class="font-size-12"
-                        data-toggle="tooltip"
-                        data-placement="top"
-                        title="Amount"
-                      >
-                        <i class="bx bxs-group"></i> 0 followers
-                      </h5>
-                    </li>
-                    <li class="list-inline-item me-3">
-                      <h5
-                        class="font-size-12"
-                        data-toggle="tooltip"
-                        data-placement="top"
-                        title="Amount"
-                      >
-                        <i class="bx bx bx-share-alt"></i> 0
-                      </h5>
-                    </li>
-                    <li class="list-inline-item me-3">
-                      <h5
-                        class="font-size-12"
-                        data-toggle="tooltip"
-                        data-placement="top"
-                        title="Amount"
-                      >
-                        <i class="bx bx-star"></i> 0
-                        <!--图标变黄色<i class="bx bx-star text-warning"></i> 0-->
-                      </h5>
-                    </li>
-                  </ul>
-              </div>
-
-            </div>
-          </div>
-          <!--左上角带头像的框结束-->
-
-          <!--左下角个人信息-->
-          <div class="card">
-            <div class="card-body">
-              <div class="d-flex align-items-start">
-                <h4 class="card-title mb-4" style="margin:5px 0 4px 0 !important">个人信息</h4>
-                <div class="ms-auto">
-                  <!--<a href class="btn btn-primary btn-sm" style="color:#242922e; background-color:#fafbfc; border-color:rgbs(27,31,35,0.15); box-shadow:0 1px rgba(27,31m35,0.04)">-->
-                  <a href class="btn btn-sm" style="color:#242922e; background-color:#eff2f7; border-color:rgbs(27,31,35,0.15); box-shadow:0 1px rgba(27,31m35,0.04)">
-                    编辑
-                  </a>
-                </div>
-                <!--<b-button
-                  class="btn btn-success ms-auto"
-                  @click="showModal = true"
-                  >编辑</b-button
-                >-->
-              </div>
-              <!--<p>个人简介</p>-->
-              <div class="table-responsive ">
-                <table class="table table-nowrap mb-0">
-                  <tbody>
-                    <tr>
-                      <th scope="row">用户名</th>
-                      <td>{{userInfo.username}}</td>
-                    </tr>
-                    <tr v-if="userInfo.name">
-                      <th scope="row">用户姓名</th>
-                      <td>{{userInfo.name}}</td>
-                    </tr>
-                    <tr v-if="userInfo.blog">
-                      <th scope="row">我的博客</th>
-                      <td>{{userInfo.blog}}</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">邮箱</th>
-                      <td>{{userInfo.email}}</td>
-                    </tr>
-                    <tr v-if="userInfo.associatedAcount">
-                      <th scope="row">关联账号</th>
-                      <td>{{userInfo.associatedAcount}}</td>
-
-                    </tr>
-                    <tr>
-                      <th scope="row">用户权限</th>
-                      <td>
-                        <div>
-                          <a
-                            href="javascript: void(0);"
-                            class="badge bg-primary font-size-11"
-                            >管理员</a
-                          >
-                        </div>
-                      </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">我的团队</th>
-                        <td> </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-          <!--左下角个人信息结束-->
-
-        </div>
-        <!--左边这一列结束-->
+     
 
         <!--右边这一列-->
         <div class="col-xl-8">
@@ -230,6 +94,183 @@ export default {
         </div>
         <!--右边这一列结束-->
 
+
+        <!--左边这一列-->
+        <div class="col-xl-4">
+          <div class="card">
+            <div class="card-body">
+              <div class="media align-items-center">
+                <div class="align-self-center me-3">
+                  <img
+                    src="@/assets/images/users/avatar-1.jpg"
+                    v-real-img="userInfo.avatar_url"
+                    class="avatar-xs rounded-circle"
+                    alt
+                  />
+                </div>
+                <div class="media-body">
+                  <h5 class="font-size-15 mt-0 mb-1">
+                    {{ userInfo.name }}
+                  </h5>
+                </div>
+              </div>
+              <hr class="mt-2" />
+               
+              <p class="text-muted" v-if="isAdmin">
+                <a
+                  href="javascript: void(0);"
+                  class="badge bg-primary font-size-11"
+                  >管理员</a
+                >
+              </p>
+              <!--<p class="text-muted">这个人很懒，什么也没有留下~</p>-->
+              <p v-if="userInfo.email" class="text-muted">{{userInfo.email}}</p>
+              <p v-if="userInfo.blog" class="text-muted">{{userInfo.blog}}</p>
+              <p v-if="userInfo.username" class="text-muted">{{userInfo.username}}</p>
+              <p v-if="userInfo.associatedAcount" class="text-muted">{{userInfo.associatedAcount}}</p>
+              
+              <p class="text-muted">
+                <ul class="list-inline mb-0">
+                    <li class="list-inline-item me-3">
+                      <h5
+                        class="font-size-12"
+                        data-toggle="tooltip"
+                        data-placement="top"
+                        title="Amount"
+                      >
+                        <i class="bx bxs-group"></i> 0 followers
+                      </h5>
+                    </li>
+                    <li class="list-inline-item me-3">
+                      <h5
+                        class="font-size-12"
+                        data-toggle="tooltip"
+                        data-placement="top"
+                        title="Amount"
+                      >
+                        <i class="bx bx bx-share-alt"></i> 0 
+                      </h5>
+                    </li>
+                    <li class="list-inline-item me-3">
+                      <h5
+                        class="font-size-12"
+                        data-toggle="tooltip"
+                        data-placement="top"
+                        title="Amount"
+                      >
+                        <i class="bx bx-star"></i> 0
+                        <!--图标变黄色<i class="bx bx-star text-warning"></i> 0-->
+                      </h5>
+                    </li>
+                  </ul>
+              </p>
+              <a
+                class="btn btn-outline-primary btn-md btn-item mb-2"
+                download="docker-compose-config"
+                style="width:90%; padding:4px 16px"
+              >
+                <!--<i class="bx bx-cloud-download font-size-16 align-middle me-1"></i>-->
+                编辑
+              </a>
+
+
+             
+            </div>
+          </div>
+          <!--左上角带头像的框-->
+          <div class="card text-center">
+            <div class="card-body">
+              <div v-if="!avatarUrl" class="avatar-sm mx-auto mb-4">
+                <span
+                  class="avatar-title rounded-circle bg-soft bg-primary text-primary font-size-16"
+                  >{{userInfo.name}}</span
+                >
+              </div>
+              <div v-if="avatarUrl" class="mb-4">
+                <img
+                  class="rounded-circle avatar-sm"
+                  :src="`${avatarUrl}`"
+                  alt
+                />
+              </div>
+              <h5 class="font-size-15 mb-1">
+                <p>
+                  <a href="javascript: void(0);" class="text-dark">{{
+                    userInfo.name
+                  }}     </a>
+                  <a  v-if="isAdmin"
+                    href="javascript: void(0);"
+                    class="badge bg-primary font-size-11 flex"
+                    >管理员</a
+                  >
+                </p>
+              </h5>
+              <p class="text-muted">这个人很懒，什么也没有留下~</p>
+              <p v-if="userInfo.email" class="text-muted">{{userInfo.email}}</p>
+              <p v-if="userInfo.blog" class="text-muted">{{userInfo.blog}}</p>
+              <p v-if="userInfo.username" class="text-muted">{{userInfo.username}}</p>
+              <p v-if="userInfo.associatedAcount" class="text-muted">{{userInfo.associatedAcount}}</p>
+              
+              <!--
+              <div>
+                <div class="mt-4">
+                  <a href class="btn btn-primary btn-sm">
+                    Edit Profile
+                  </a>
+                </div>
+              </div>
+              -->
+              
+              <a
+                class="btn btn-outline-primary btn-md btn-item mb-2"
+                download="docker-compose-config"
+                style="width:100%; padding:4px 16px"
+              >
+                <!--<i class="bx bx-cloud-download font-size-16 align-middle me-1"></i>-->
+                编辑
+              </a>
+
+              <div class="text-muted">
+                <ul class="list-inline mb-0">
+                    <li class="list-inline-item me-3">
+                      <h5
+                        class="font-size-12"
+                        data-toggle="tooltip"
+                        data-placement="top"
+                        title="Amount"
+                      >
+                        <i class="bx bxs-group"></i> 0 followers
+                      </h5>
+                    </li>
+                    <li class="list-inline-item me-3">
+                      <h5
+                        class="font-size-12"
+                        data-toggle="tooltip"
+                        data-placement="top"
+                        title="Amount"
+                      >
+                        <i class="bx bx bx-share-alt"></i> 0 
+                      </h5>
+                    </li>
+                    <li class="list-inline-item me-3">
+                      <h5
+                        class="font-size-12"
+                        data-toggle="tooltip"
+                        data-placement="top"
+                        title="Amount"
+                      >
+                        <i class="bx bx-star"></i> 0
+                        <!--图标变黄色<i class="bx bx-star text-warning"></i> 0-->
+                      </h5>
+                    </li>
+                  </ul>
+              </div>
+            </div>
+          </div>
+          <!--左上角带头像的框结束-->
+
+        </div>
+        <!--左边这一列结束-->
       </div>
       
     </Layout>
