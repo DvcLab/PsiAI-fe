c493ffcdc0dba93ef38549933a0fb22d54fd5b48
刘芊怡 <liuqianyi16@163.com>
2021-05-17 17:27:14 +0800

    perf
    大小屏幕切换的时候，让个人信息那个框显示在上面

--- src/router/views/DvcAI/user/profile.vue
+++ src/router/views/DvcAI/user/profile.vue
@@ -53,38 +53,118 @@ export default {
     <Layout>
       <PageHeader :title="title" :items="items" />
       <div class="row">
-
+        <!--右边这一列(小窗口时才显示)-->
+        <div class="col-md-4 d-md-none">
+          <div class="card text-center">
+            <div class="card-body">
+              <div v-if="!avatarUrl" class="avatar-sm mx-auto mb-4">
+                <span
+                  class="avatar-title rounded-circle bg-soft bg-primary text-primary font-size-16"
+                  >{{userInfo.name}}</span
+                >
+              </div>
+              <div v-if="avatarUrl" class="mb-4">
+                <img
+                  class="rounded-circle avatar-sm"
+                  :src="`${avatarUrl}`"
+                  alt
+                />
+              </div>
+              <h5 class="font-size-15 mb-1">
+                <p>
+                  <a href="javascript: void(0);" class="text-dark">{{
+                    userInfo.name
+                  }}</a>
+                  <a  v-if="isAdmin"
+                    href="javascript: void(0);"
+                    class="badge bg-primary font-size-11 flex"
+                    >管理员</a
+                  >
+                </p>
+              </h5>
+              <p class="text-muted">这个人很懒，什么也没有留下~</p>
+              <p v-if="userInfo.email" class="text-muted">{{userInfo.email}}</p>
+              <p v-if="userInfo.blog" class="text-muted">{{userInfo.blog}}</p>
+              <p v-if="userInfo.username" class="text-muted">{{userInfo.username}}</p>
+              <p v-if="userInfo.associatedAcount" class="text-muted">{{userInfo.associatedAcount}}</p>
+              <!--
+              <a
+                class="btn btn-outline-primary btn-md btn-item mb-2"
+                download="docker-compose-config"
+                style="width:100%; padding:4px 16px"
+              >
+                编辑
+              </a>
+              -->
+              <div class="text-muted">
+                <ul class="list-inline mb-0">
+                  <li class="list-inline-item me-3">
+                    <h5
+                      class="font-size-12"
+                      data-toggle="tooltip"
+                      data-placement="top"
+                      title="Amount"
+                    >
+                      <i class="bx bxs-group"></i> 0 followers
+                    </h5>
+                  </li>
+                  <li class="list-inline-item me-3">
+                    <h5
+                      class="font-size-12"
+                      data-toggle="tooltip"
+                      data-placement="top"
+                      title="Amount"
+                    >
+                      <i class="bx bx bx-share-alt"></i> 0 
+                    </h5>
+                  </li>
+                  <li class="list-inline-item me-3">
+                    <h5
+                      class="font-size-12"
+                      data-toggle="tooltip"
+                      data-placement="top"
+                      title="Amount"
+                    >
+                      <i class="bx bx-star"></i> 0
+                    </h5>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--右边这一列结束-->
         <!--左边这一列-->
-        <div class="col-xl-8">
+        <div class="col-md-8">
           <div class="card">
-                <div class="card-body">
-                    <b-tabs content-class="p-3" nav-class="nav-tabs-custom">
-                        <b-tab title="项目" active>
-                          我的项目
-                        </b-tab>
-                        <b-tab title="数据集" active>
-                          我的数据集
-                        </b-tab>
-                        <b-tab title="镜像" active>
-                          我的镜像
-                        </b-tab>
-                        <b-tab title="容器" active>
-                          我的容器
-                        </b-tab>
-                        <b-tab title="收藏" active>
-                          我收藏的项目/数据集/镜像/容器
-                        </b-tab>
-                        <b-tab title="关注" active>
-                          我的关注
-                        </b-tab>
-                    </b-tabs>
-                </div>
+            <div class="card-body">
+              <b-tabs content-class="p-3" nav-class="nav-tabs-custom">
+                <b-tab title="项目" active>
+                  我的项目
+                </b-tab>
+                <b-tab title="数据集" active>
+                  我的数据集
+                </b-tab>
+                <b-tab title="镜像" active>
+                  我的镜像
+                </b-tab>
+                <b-tab title="容器" active>
+                  我的容器
+                </b-tab>
+                <b-tab title="收藏" active>
+                  我收藏的项目/数据集/镜像/容器
+                </b-tab>
+                <b-tab title="关注" active>
+                  我的关注
+                </b-tab>
+              </b-tabs>
+            </div>
           </div>
         </div>
         <!--左边这一列结束-->
 
         <!--右边这一列-->
-        <div class="col-xl-4">
+        <div class="col-md-4 d-none d-md-block">
           <div class="card text-center">
             <div class="card-body">
               <div v-if="!avatarUrl" class="avatar-sm mx-auto mb-4">
@@ -117,7 +197,7 @@ export default {
               <p v-if="userInfo.blog" class="text-muted">{{userInfo.blog}}</p>
               <p v-if="userInfo.username" class="text-muted">{{userInfo.username}}</p>
               <p v-if="userInfo.associatedAcount" class="text-muted">{{userInfo.associatedAcount}}</p>
-
+              <!--
               <a
                 class="btn btn-outline-primary btn-md btn-item mb-2"
                 download="docker-compose-config"
@@ -125,40 +205,40 @@ export default {
               >
                 编辑
               </a>
-
+              -->
               <div class="text-muted">
                 <ul class="list-inline mb-0">
-                    <li class="list-inline-item me-3">
-                      <h5
-                        class="font-size-12"
-                        data-toggle="tooltip"
-                        data-placement="top"
-                        title="Amount"
-                      >
-                        <i class="bx bxs-group"></i> 0 followers
-                      </h5>
-                    </li>
-                    <li class="list-inline-item me-3">
-                      <h5
-                        class="font-size-12"
-                        data-toggle="tooltip"
-                        data-placement="top"
-                        title="Amount"
-                      >
-                        <i class="bx bx bx-share-alt"></i> 0 
-                      </h5>
-                    </li>
-                    <li class="list-inline-item me-3">
-                      <h5
-                        class="font-size-12"
-                        data-toggle="tooltip"
-                        data-placement="top"
-                        title="Amount"
-                      >
-                        <i class="bx bx-star"></i> 0
-                      </h5>
-                    </li>
-                  </ul>
+                  <li class="list-inline-item me-3">
+                    <h5
+                      class="font-size-12"
+                      data-toggle="tooltip"
+                      data-placement="top"
+                      title="Amount"
+                    >
+                      <i class="bx bxs-group"></i> 0 followers
+                    </h5>
+                  </li>
+                  <li class="list-inline-item me-3">
+                    <h5
+                      class="font-size-12"
+                      data-toggle="tooltip"
+                      data-placement="top"
+                      title="Amount"
+                    >
+                      <i class="bx bx bx-share-alt"></i> 0 
+                    </h5>
+                  </li>
+                  <li class="list-inline-item me-3">
+                    <h5
+                      class="font-size-12"
+                      data-toggle="tooltip"
+                      data-placement="top"
+                      title="Amount"
+                    >
+                      <i class="bx bx-star"></i> 0
+                    </h5>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
